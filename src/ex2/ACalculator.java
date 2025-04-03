3704d476955b45d2288a03ebc37564f3ea2561e9
cristinaa17 <cristinamaria.radulescu@ulbsibiu.ro>
2025-04-03 10:05:03 +0300

    lab6

---
+++ src/ex2/ACalculator.java
@@ -0,0 +1,17 @@
+package ex2;
+
+public abstract class ACalculator {
+    protected Object state;
+
+    public Object result() {
+        return state;
+    }
+
+    public ACalculator clear() {
+        this.init();
+        return this;
+    }
+
+    protected abstract void init();
+
+}
